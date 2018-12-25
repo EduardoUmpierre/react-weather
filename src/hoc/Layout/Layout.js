c99212c932a0da7b8a8d111d305251274c1f8ca2
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2018-12-25 08:17:18 -0200

    Layout component added

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from './Layout.module.css'
+
+const layout = props => {
+    return (
+        <>
+            <header className={classes.header}>
+                <h2>{props.city}</h2>
+            </header>
+
+            <main>{props.children}</main>
+        </>
+    );
+};
+
+export default layout;
