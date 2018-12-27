331a54a31555dd1966b5e7d08958eb989e7bf83b
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2018-12-27 08:25:04 -0200

    Moved to containers folder

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,26 +1,60 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-import Layout from '../../hoc/Layout/Layout';
-import CurrentWeather from '../../components/CurrentWeather/CurrentWeather';
-import WeeklyForecast from '../../components/WeeklyForecast/WeeklyForecast';
+import WeatherAPI from '../../utils/WeatherAPI'
+import Layout from '../../hoc/Layout/Layout'
+import CurrentWeather from '../../components/CurrentWeather/CurrentWeather'
+import WeeklyForecast from '../../components/WeeklyForecast/WeeklyForecast'
 
 class App extends Component {
     state = {
         city: 'Porto Alegre',
-        weeklyForecast: []
-    };
+        weather: null
+    }
+
+    componentDidMount = () => {
+        WeatherAPI.getCurrentWeather(this.state.city).then(response => {
+            this.setState(
+                {
+                    weather: this.getWeatherDataFromCurrentWeather(
+                        response.data
+                    )
+                },
+                () => console.log(this.state.weather)
+            )
+        })
+    }
+
+    getWeatherDataFromCurrentWeather(data) {
+        const additionalData = [...data.weather]
+            .map(weather => ({
+                class: weather.main,
+                description: weather.description
+            }))
+            .concat(
+                [{ ...data.sys }].map(data => ({
+                    sunrise: data.sunrise,
+                    sunset: data.sunset
+                }))
+            )
+            .reduce((data, el) => ({ ...data, ...el }), {})
+
+        return {
+            ...data.main,
+            ...additionalData
+        }
+    }
 
     render() {
         return (
             <div className="App">
                 <Layout city={this.state.city}>
-                    <CurrentWeather city={this.state.city} />
+                    <CurrentWeather weather={this.state.weather} />
                     <WeeklyForecast forecast={this.state.weeklyForecast} />
                 </Layout>
             </div>
-        );
+        )
     }
 }
 
-export default App;
+export default App
